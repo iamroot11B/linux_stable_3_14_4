49794d7c5a0f46c02f4c765b8ae18ffdaffcc43b
seungkikim <tttuuu888@gmail.com>
2014-08-09 15:36:34 +0900

    주석 위치 조정(define 밖으로..)

--- arch/arm/include/asm/cputype.h
+++ arch/arm/include/asm/cputype.h
@@ -79,15 +79,15 @@
 
 extern unsigned int processor_id;
 
+/*! 
+ * asm ( code : output operand list : input operand list : clobber list );  
+ * clobber list: clobber는 asm를 실행 시, 값이 변하는 것을 적어준다.
+ * http://codecat.tistory.com/entry/%EC%9D%B8%EB%9D%BC%EC%9D%B8-%EC%96%B4%EC%85%88%EB%B8%94%EB%A6%AC
+ */
 #ifdef CONFIG_CPU_CP15
 #define read_cpuid(reg)							\
 	({								\
 		unsigned int __val;					\
-      /*!
-       * asm ( code : output operand list : input operand list : clobber list );
-       * clobber list: clobber는 asm를 실행 시, 값이 변하는 것을 적어준다. 
-	   * http://codecat.tistory.com/entry/%EC%9D%B8%EB%9D%BC%EC%9D%B8-%EC%96%B4%EC%85%88%EB%B8%94%EB%A6%AC
-       */
      asm("mrc	p15, 0, %0, c0, c0, " __stringify(reg)	\
 		    : "=r" (__val)					\
 		    :							\
