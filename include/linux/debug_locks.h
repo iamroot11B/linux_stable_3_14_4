2112f43f124451f421a4b54f5b08a5be176a71a6
SeungKi Kim <tttuuu888@gmail.com>
2017-08-13 21:18:59 +0900

    2017-08-12 study - compile error fix

--- include/linux/debug_locks.h
+++ include/linux/debug_locks.h
@@ -10,7 +10,7 @@ struct task_struct;
 extern int debug_locks;
 extern int debug_locks_silent;
 
-ocks.c
+/*! 2017. 8.12 study -ing */
 static inline int __debug_locks_off(void)
 {
 	return xchg(&debug_locks, 0);
